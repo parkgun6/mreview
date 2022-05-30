b31a8e2521b23a2a486e8ae5d58bf46fed75f4af
parkgun6 <a01088807068@gmail.com>
2022-05-30 14:31:13 +0900

    feat: 리스트조회, 단건 조회 추가 1. getListPage 2. getMovieWithAll

--- src/main/java/org/geon/mreview/repository/MovieRepository.java
+++ src/main/java/org/geon/mreview/repository/MovieRepository.java
@@ -1,7 +1,29 @@
 package org.geon.mreview.repository;
 
 import org.geon.mreview.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m " +
+            "left join MovieImage mi on mi.movie = m " +
+            // 영화 이미지 중 가장 나중에 추가된 이미지를 가져오는 쿼리
+            "and mi.inum = (select max(mi2.inum) from MovieImage mi2 where mi2.movie = m) " +
+            "left join Review r on r.movie = m " +
+            "group by m")
+    Page<Object[]> getListPage(Pageable pageable);
+
+    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m " +
+            "left join MovieImage mi on mi.movie = m " +
+            "left join Review r on r.movie = m " +
+            "where m.mno = :mno " +
+            "group by mi")
+    List<Object[]> getMovieWithAll(@Param("mno") Long mno);
 }
