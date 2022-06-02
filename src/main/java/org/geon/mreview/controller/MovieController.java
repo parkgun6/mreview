b882833b8d4563b12ff6d3f61101c1b76128f5fe
parkgun6 <a01088807068@gmail.com>
2022-06-02 13:05:29 +0900

    feat: MovieController 생성 1. register

---
+++ src/main/java/org/geon/mreview/controller/MovieController.java
@@ -0,0 +1,35 @@
+package org.geon.mreview.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.geon.mreview.dto.MovieDTO;
+import org.geon.mreview.service.MovieService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@Log4j2
+@RequestMapping("/movie")
+@RequiredArgsConstructor
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @GetMapping("/register")
+    public void register() {
+    }
+
+    @PostMapping("/register")
+    public String register(MovieDTO movieDTO, RedirectAttributes redirectAttributes) {
+        log.info("MovieDTO : " + movieDTO);
+
+        Long mno = movieService.register(movieDTO);
+
+        redirectAttributes.addFlashAttribute("msg", mno);
+
+        return "redirect:/movie/list";
+    }
+}
