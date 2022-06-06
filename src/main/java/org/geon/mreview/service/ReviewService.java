a33def3fa43ba063d293d54e03c55fcfedd65c7f
parkgun6 <a01088807068@gmail.com>
2022-06-06 23:00:46 +0900

    feat: ReviewService 생성 1. getListOfMovie 2. register 3. modify 4. remove 5. dtoToEntity 6. entityToDTO

---
+++ src/main/java/org/geon/mreview/service/ReviewService.java
@@ -0,0 +1,51 @@
+package org.geon.mreview.service;
+
+import org.geon.mreview.dto.ReviewDTO;
+import org.geon.mreview.entity.Member;
+import org.geon.mreview.entity.Movie;
+import org.geon.mreview.entity.Review;
+
+import java.util.List;
+
+public interface ReviewService {
+
+    List<ReviewDTO> getListOfMovie(Long mno);
+
+    Long register(ReviewDTO movieReviewDTO);
+
+    //특정한 영화리뷰 수정
+    void modify(ReviewDTO movieReviewDTO);
+
+    //영화 리뷰 삭제
+    void remove(Long reviewnum);
+
+    default Review dtoToEntity(ReviewDTO movieReviewDTO){
+
+        Review movieReview = Review.builder()
+                .reviewnum(movieReviewDTO.getReviewnum())
+                .movie(Movie.builder().mno(movieReviewDTO.getMno()).build())
+                .member(Member.builder().mid(movieReviewDTO.getMid()).build())
+                .grade(movieReviewDTO.getGrade())
+                .text(movieReviewDTO.getText())
+                .build();
+
+        return movieReview;
+    }
+
+    default ReviewDTO entityToDto(Review movieReview){
+
+        ReviewDTO movieReviewDTO = ReviewDTO.builder()
+                .reviewnum(movieReview.getReviewnum())
+                .mno(movieReview.getMovie().getMno())
+                .mid(movieReview.getMember().getMid())
+                .nickname(movieReview.getMember().getNickname())
+                .email(movieReview.getMember().getEmail())
+                .grade(movieReview.getGrade())
+                .text(movieReview.getText())
+                .regDate(movieReview.getRegDate())
+                .modDate(movieReview.getModDate())
+                .build();
+
+        return movieReviewDTO;
+    }
+}
